eef4e610608aa5f67e10b47cd2422e68966f99fd
Luis <machadoluismendes@gmail.com>
2019-12-31 19:19:33 -0300

    porta wamp

--- index.php
+++ index.php
@@ -1,39 +1,93 @@
 <!DOCTYPE html>
-<html>
-<head></head>
+<html lang="pt-bR">
+<head>
+    <title>Vendedores</title>
+</head>
 <body>
 
-<header>
-<h1> Empresa Binária <h1>
-<h2>Sistema Plano de Carreira Vendedores</h2>
-</header>
+
+<div>
+    <header>
+        <h1> Empresa Binária</h1>
+        <h2>Sistema Plano de Carreira Vendedores</h2>
+    </header>
+</div>
 
 <p id="demo"></p>
 <p id="demo2"></p>
 
 
 <script>
-var obj = JSON.parse('{ "id":"1", "nome":"Vendedor 1", "pt":"0 pt", "esquerda":"null", "direita":"null"}');
-document.getElementById("demo").innerHTML = obj.nome + ", " + obj.pt + ", Perna Esquerda: " + obj.esquerda + ", Perna direita: " + obj.direita ;
+    var obj = JSON.parse('{ "id":"1", "nome":"Vendedor 1", "pt":"0 pt", "esquerda":"null", "direita":"null"}');
+    document.getElementById("demo").innerHTML = obj.nome + ", " + obj.pt + ", Perna Esquerda: " + obj.esquerda + ", Perna direita: " + obj.direita;
 
-var obj2 = JSON.parse('{ "id":"1", "nome":"Vendedor 1", "pt":"0 pt", "esquerda":"null", "direita":"null"}');
-document.getElementById("demo2").innerHTML = obj.nome + ", " + obj.pt + ", Perna Esquerda: " + obj.esquerda + ", Perna direita: " + obj.direita ;
+    var obj2 = JSON.parse('{ "id":"1", "nome":"Vendedor 1", "pt":"0 pt", "esquerda":"null", "direita":"null"}');
+    document.getElementById("demo2").innerHTML = obj.nome + ", " + obj.pt + ", Perna Esquerda: " + obj.esquerda + ", Perna direita: " + obj.direita;
 
 </script>
 
 <?php
 
-echo 'Hello World  PHP!';
+echo 'Hello World  PHPMyAdmin!';
 
-if ($db->getAttribute(PDO::ATTR_DRIVER_NAME) == 'mysql') {
-    $stmt = $db->prepare('select * from foo',
-        array(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true));
-} else {
-    die("my application only works with mysql; I should use \$stmt->fetchAll() instead");
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Nome</th><th>Pontos</th><th>PernaEsquerda</th><th>PernaDireita</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator
+{
+    function __construct($it)
+    {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current()
+    {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current() . "</td>";
+    }
+
+    function beginChildren()
+    {
+        echo "<tr>";
+    }
+
+    function endChildren()
+    {
+        echo "</tr>" . "\n";
+    }
 }
 
-#$mysqli = mysqli_connect('127.0.0.0.1','root','','plano_carreira_vendedores','3308');
-#echo 'procedural';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "plano_carreira_vendedores";
+//3308 wamp 3306 workbench
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=3308;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("use plano_carreira_vendedores");
+    $stmt->execute();
+
+    //echo "<p>database changed</p>";
+    print_r("database changed");
+
+    $stmt = $conn->prepare("SELECT * FROM vendedores");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+        echo $v;
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+
+echo '<br>';
+echo 'Hello World  Workbench MySQL Server Community!';
 
 ?>
 </body>
